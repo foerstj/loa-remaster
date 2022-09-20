cb0723300f690f58f2b3ad17ec9aa57dc5e12aa9
Johannes Förstner <johannes@famfoerstner.de>
2022-09-20 20:39:49 +0200

    Add README, build scripts, gitignore files

---
+++ README.md
@@ -0,0 +1,20 @@
+# LoA remaster
+
+The original Legends of Aranna map, remastered.
+
+## What I did
+
+TBD
+
+## How to build
+
+GasPy repo: https://github.com/foerstj/gaspy
+
+- Put TankCreator and gaspy on the same folder level as your Dungeon Siege installation. Basically in the upper folder, you should have a "Dungeon Siege" folder, a "TankCreator" folder, and a "gaspy" folder.
+- Put this repo as "Bits" into your personal Dungeon Siege LoA folder (where the savegames & screenshots are).
+- Go into your Dungeon Siege installation folder and open CMD there.
+- %USERPROFILE%\Documents\Dungeon Siege LoA\Bits\build.bat
+
+## Attribution
+
+This map was created by Gas-Powered Games, not me. I'm just doing minor changes here.
---
+++ build.bat
@@ -0,0 +1,37 @@
+:: This script is supposed to be executed from your DS installation folder.
+:: TankCreator and gaspy are expected to be in sibling dirs.
+
+:: name of map
+set map=legends-of-aranna
+:: name of map, case-sensitive
+set map_cs=Legends of Aranna
+:: path of DSLOA documents dir (where Bits are)
+set doc_dsloa=%USERPROFILE%\Documents\Dungeon Siege LoA
+:: path of DS installation
+set ds=.
+:: path of TankCreator
+set tc=..\TankCreator
+:: path of GasPy
+set gaspy=..\gaspy
+
+:: param
+set mode=%1
+echo %mode%
+
+:: Compile map file
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
+pushd %gaspy%
+venv\Scripts\python -m build.fix_start_positions_required_levels %map% "%tmp%\Bits"
+if %errorlevel% neq 0 pause
+::SETLOCAL EnableDelayedExpansion
+::if not "%mode%"=="light" (
+::  venv\Scripts\python -m build.add_world_levels %map% "%tmp%\Bits" "%doc_dsloa%\Bits"
+::  if !errorlevel! neq 0 pause
+::)
+popd
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Maps\%map_cs%.dsmap" -copyright "GPG 2003" -title "%map_cs%" -author "Johannes Förstner"
+if %errorlevel% neq 0 pause
+
+:: Cleanup
+rmdir /S /Q "%tmp%\Bits"
