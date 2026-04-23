b66fff0e56b0e7469343a46ca63ace7965c62fe6
Johannes Förstner <johannes@famfoerstner.de>
2026-04-23 16:57:39 +0200

    Add a cleanup.bat

---
+++ cleanup.bat
@@ -0,0 +1,8 @@
+:: names
+set map_cs=Legends of Aranna
+
+:: path of DS installation
+set ds=%DungeonSiege%
+
+:: Cleanup resources so as not to confuse Siege Editor
+del "%ds%\DSLOA\%map_cs%.dsmap"
